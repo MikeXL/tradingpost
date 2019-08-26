747859948f163918df528a45c4a7cc1587d42104
M <mike@mscanada.net>
2019-08-25 23:07:15 -0700

    sma

--- R/trade.R
+++ R/trade.R
@@ -13,6 +13,14 @@ chartSeries(MSFT[backtest_period], TA='addMACD();addRSI();addCCI();addDEMA();add
 # trading strategies
 # return buy and sell signal 1, -1
 #
+# SMA trading strategy
+strat_sma <- function(x){
+  sma12 <- SMA(x, 12)
+  sma26 <- SMA(x, 26)
+  signal <- lag(ifelse(sma12 < sma26, -1, 1))
+  signal
+}
+
 # MACD trading strategy
 strat_macd <- function(x){
   macd   <- MACD(Cl(x), 12, 26, 9, SMA, F)
@@ -54,16 +62,18 @@ strat_nnet <- function(prices) {
 
 # backtest with MSFT 2018 stock, closing price
 
-data = Cl(MSFT)
+data = Cl(SPY)
 returns_macd     <- ROC(data) * strat_macd(data)
 returns_rsi_cci  <- ROC(data) * strat_rsi_cci(data)
 returns_dvi      <- ROC(data) * strat_dvi(data)
+returns_sma      <- ROC(data) * strat_sma(data)
 returns_dema     <- ROC(data) * strat_dema(data)
 returns_base     <- dailyReturn(data)
 
 rets_macd        <- returns_macd    [backtest_period]
 rets_rsi_cci     <- returns_rsi_cci [backtest_period]
 rets_dvi         <- returns_dvi     [backtest_period]
+rets_sma         <- returns_sma     [backtest_period]
 rets_dema        <- returns_dema    [backtest_period]
 rets_base        <- returns_base    [backtest_period]
 
@@ -71,11 +81,12 @@ rets_base        <- returns_base    [backtest_period]
 names(rets_macd)     <- "MACD"
 names(rets_rsi_cci)  <- "RSI CCI"
 names(rets_dvi)      <- "DVI"
+names(rets_sma)      <- "SMA"
 names(rets_dema)     <- "DEMA"
 names(rets_base)     <- "daily return"
 
 charts.PerformanceSummary(
-    cbind( rets_macd, rets_base, rets_dvi, rets_rsi_cci, rets_dema), 
+    cbind( rets_macd, rets_base, rets_dvi, rets_sma, rets_rsi_cci, rets_dema), 
     main="Performance of Strategies"
 )
 
